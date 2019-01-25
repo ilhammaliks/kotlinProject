0d1d219db9b13347b33798fe30b1970f9ce70253
Zhemus <ilhammalikmuhammad@gmail.com>
2019-01-25 19:01:35 +0700

    initial commit

---
+++ app/src/main/java/com/project/kotlinproject/Item.kt
@@ -0,0 +1,3 @@
+package com.project.kotlinproject
+
+data class Item (val name: String?, val image: Int?)
---
+++ app/src/main/java/com/project/kotlinproject/RecyclerViewAdapter.kt
@@ -0,0 +1,34 @@
+package com.project.kotlinproject
+
+import android.content.Context
+import android.support.v7.widget.RecyclerView
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.ImageView
+import android.widget.TextView
+import com.squareup.picasso.Picasso
+
+class RecyclerViewAdapter(private val context: Context, private val items: List<Item>) :
+    RecyclerView.Adapter<RecyclerViewAdapter.ViewHolder>() {
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int) =
+        ViewHolder(LayoutInflater.from(context).inflate(R.layout.item_list, parent, false))
+
+    override fun getItemCount(): Int = items.size
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        holder.bindItem(items[position])
+    }
+
+    class ViewHolder(view: View) : RecyclerView.ViewHolder(view) {
+
+        private val name_club = view.findViewById<TextView>(R.id.club_name)
+        private val logo_club = view.findViewById<ImageView>(R.id.club_logo)
+
+        fun bindItem(items: Item) {
+            name_club.text = items.name
+            items.image?.let { Picasso.get().load(it).into(logo_club) }
+        }
+    }
+
+}
